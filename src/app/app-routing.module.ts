3fb3d3195b68ed73c3d2af99a54134910e02ce86
diana <diana.carhuamanta@deister.es>
2023-04-23 02:05:22 -0500

    router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { WorksComponent } from './pages/works/works.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent
+  },
+  {
+    path: 'works',
+    component: WorksComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './profile/header/header.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { WorksComponent } from './pages/works/works.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    WorksComponent,
+    ContactComponent
+  ]
 })
 export class AppModule { }
